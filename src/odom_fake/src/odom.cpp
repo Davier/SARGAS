c05a1ca97ae4ffd59b8343e662f6d3f164ea529c
Brice DAVIER <bricedavier@gmail.com>
2015-01-30 19:26:03 +0100

    Merge with dropbox : launch files work

---
+++ src/odom_fake/src/odom.cpp
@@ -0,0 +1,79 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <fstream>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <cmath>
+#include <string>
+
+int stoi(std::string Text)
+{
+ int result=0;
+ std::istringstream convert(Text);
+ if (!(convert >> result))
+	result=0;
+ return(result);
+}
+
+int main(int argc,char** argv){
+	ros::init(argc, argv, "odometry_publisher");
+	ros::NodeHandle n;
+	ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom",50);
+	ros::Rate r(1.0);
+	std::string raw_st[2];
+	double position_actuelle_gauche=0, position_actuelle_droit=0;
+	double position_passee_gauche=0, position_passee_droit=0;
+	double delta_gauche=0,delta_droit=0;
+	double temps_passe=0,interval=0;
+	double x=0,y=0,th=0;
+	double vx=0,vy=0,vth=0;
+	const double pi=1*atan(1);
+	const double nb_strip=32;
+	const double rayon=69.0/2.0*0.001;//in m
+	const double espacement=0.240;//ecart entre les roues en m
+
+	nav_msgs::Odometry odom;
+	odom.header.frame_id="odom"; 
+	odom.child_frame_id="base_link";
+	
+	std::ifstream codeur_gauche;
+	codeur_gauche.open("/home/florian/odom_1");
+	std::ifstream codeur_droit;
+	codeur_droit.open("/home/florian/odom_2");
+	
+	
+	while(n.ok())
+	{
+		odom.header.stamp=ros::Time::now();
+		interval=odom.header.stamp.toSec()-temps_passe;
+		temps_passe=odom.header.stamp.toSec();
+		
+		codeur_gauche >> raw_st[0];
+		codeur_droit >> raw_st[1];
+		
+		position_actuelle_gauche=stoi(raw_st[0]);
+		position_actuelle_droit=stoi(raw_st[1]);
+		
+		delta_gauche=(position_actuelle_gauche-position_passee_gauche)*(2.0*pi/nb_strip*rayon);//distance parcourue par la roue gauche en metres apres la derniere mesure
+		delta_droit=(position_actuelle_droit-position_passee_droit)*(2.0*pi/nb_strip*rayon);//distance parcourue par la roue gauche en metres apres la derniere mesure
+		th=(delta_droit-delta_gauche)/espacement;//calcule l'orientation du robot dans le sens trigonometrique
+		x=(delta_droit+delta_gauche)/2;
+		vx=x/interval;
+		vth=th/interval;
+
+		odom.twist.twist.linear.x=vx;
+		odom.twist.twist.linear.y=0;
+		odom.twist.twist.angular.z=vth;
+		odom.pose.pose.position.x=x;
+		odom.pose.pose.position.y=0;
+		odom.pose.pose.position.z=0;
+		odom.pose.pose.orientation=tf::createQuaternionMsgFromYaw(th);
+		odom_pub.publish(odom);
+
+		position_passee_gauche=position_actuelle_gauche;
+		position_passee_droit=position_actuelle_droit;
+
+		ros::spinOnce();
+		r.sleep();
+	}
+}
---
+++ src/sharp_fake/src/sharp.cpp
@@ -0,0 +1,91 @@
+#include "ros/ros.h"
+#include "ros/time.h"
+#include <iostream>
+#include <fstream>
+#include <sensor_msgs/LaserScan.h>
+#include <cmath>
+#include <string>
+
+int stoi(std::string Text)
+{
+ int result=0;
+ std::stringstream convert(Text);
+ if (!(convert >> result))
+	result=0;
+ return(result);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sharp");
+
+  ros::NodeHandle n;
+
+  const double distance_min=0.093;//in meters
+  const double distance_max=0.108;
+  const double pi=1*atan(1);
+  double offset[3]={0.106,0.093,0.108};
+  int raw[3];
+  std::string raw_st[3];
+  float lookup[3][1024];
+  
+  ros::Publisher sharp_pub= n.advertise<sensor_msgs::LaserScan>("sharp", 50);
+  
+  ros::Rate loop_rate(10);
+  sensor_msgs::LaserScan sharp;
+  
+  sharp.header.frame_id="sharp";
+  sharp.angle_min = -pi/4.0;
+  sharp.angle_max = pi/4.0;
+  sharp.angle_increment = pi/4.0;
+  sharp.time_increment = 0;
+  sharp.range_min = 0.03+distance_min;
+  sharp.range_max = 0.4+distance_max;
+  sharp.ranges.resize(3);
+
+  for(int i=0;i<1024;i++)
+  {
+   lookup[0][i]=(1.25*10.00)*pow(i,(-0.47));
+   lookup[1][i]=(3.08*10.00)*pow(i,(-1.03));
+   lookup[2][i]=(3.57*10.00)*pow(i,(-1.06));
+  }
+  
+  std::ifstream adc_file1;
+  adc_file1.open ("/home/florian/sharp_1");
+  std::ifstream adc_file2;
+  adc_file2.open ("/home/florian/sharp_2");
+  std::ifstream adc_file3;
+  adc_file3.open ("/home/florian/sharp_3");
+  
+  while (ros::ok())
+  {
+    adc_file1 >> raw_st[0];
+    adc_file2 >> raw_st[1];
+    adc_file3 >> raw_st[2];
+    adc_file1.seekg(0);
+    adc_file2.seekg(0);
+    adc_file3.seekg(0);
+    sharp.header.stamp = ros::Time::now();
+    for(int i=0;i<3;i++)
+    {
+      raw[i]=stoi(raw_st[i]);
+	  if(raw[i]>1023)
+	{
+          raw[i]=1023;
+        }
+      else if(raw[i]<85)//a determiner
+	{
+	  raw[i]=0;
+	}
+	 
+    sharp.ranges[i]=lookup[i][raw[i]]+offset[i];
+    sharp_pub.publish(sharp);
+    }
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+
+return 0;
+}
