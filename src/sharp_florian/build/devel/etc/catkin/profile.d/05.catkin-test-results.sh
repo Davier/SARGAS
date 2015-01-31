c99c30767380fe45923f595f29b76a6e7ec5c0c3
Florian Loiseau <lesparisiensdu19@hotmail.fr>
2015-01-31 10:19:09 +0100

    change configuration files to launch fake_localization instead of AMCL

--- src/sharp_florian/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/florian/catkin_ws/src/sharp/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
